b56087aa8bb87fd17b40a5efdbcd84a1f962ba9c
NeftisDragon <gregocm1000@gmail.com>
2022-04-05 23:19:54 -0300

    Working Cart (no styles yet); WIP login page.

--- scripts/shop.js
+++ scripts/shop.js
@@ -1,72 +1,201 @@
 //Variables and Arrays.
 
-answer = false;
-let cart = []
-const dragons = []
-nameString = "\n";
-let selection;
-let choice;
-let placeholder = []
+let dragons = []
 
 class Dragon {
-    constructor(name, breed, rarity, stock, price, overview, egg) {
+    constructor(id, name, breed, rarity, price, overview, egg) {
+        this.id = id;
         this.name = name;
         this.breed = breed;
         this.rarity = rarity;
-        this.stock = parseInt(stock);
-        this.price = parseFloat(price);
-        this.overview = overview,
-        this.egg = egg
+        this.price = price;
+        this.overview = overview;
+        this.egg = egg;
+        this.quantity = 0;
+        this.totalPrice = 0;
     }
 
-    getName() {
-        return this.name;
+    getID() {
+        return this.id;
     }
 
-    getBreed() {
-        return this.breed;
+    addUnit() {
+        this.quantity++;
+        this.totalPrice = this.price * this.quantity;
     }
 
-    getRarity() {
-        return this.rarity;
-    }
-
-    getStock() {
-        return this.stock;
-    }
-
-    getPrice() {
-        return this.price;
-    }
-
-    getOverview() {
-        return this.overview;
-    }
-
-    getEgg() {
-        return this.egg;
+    removeUnit() {
+        this.quantity--;
+        this.totalPrice = this.price * this.quantity;
     }
 }
 
 //Add all the products to an Array.
 
-dragons.push(new Dragon("ARID BADLANDS", "Bloodchaser", "Rare", "25", "500", "../assets/images/Bloodchaser/Arid Badlands/Arid_Badlands_Overview.png", "../assets/images/Bloodchaser/Arid Badlands/Arid_Badlands_egg.png"));
-dragons.push(new Dragon("FERROUS MOUNTAINS", "Bloodchaser", "Common", "31", "100", "../assets/images/Bloodchaser/Ferrous Mountains/Ferrous_Mountains_Overview.png", "../assets/images/Bloodchaser/Ferrous Mountains/not_found.png"));
-dragons.push(new Dragon("FJALNARRI MINES" || "FJALNÄRRI MINES", "Nightsnagger", "Epic", "5", "1000", "../assets/images/Nightsnagger/Fjalnarri Mines/Fjalnarri_Mines_Overview.png", "../assets/images/Nightsnagger/Fjalnarri Mines/not_found.png"));
-dragons.push(new Dragon("FORGOTTEN BOG", "Nightsnagger", "Rare", "18", "500", "../assets/images/Nightsnagger/Forgotten Bog/Forgotten_Bog_Overview.png", "../assets/images/Nightsnagger/Forgotten Bog/Forgotten_Bog_Egg.png"));
-dragons.push(new Dragon("FROZEN LAKE", "Ironwing", "Epic", "6", "1000", "../assets/images/Ironwing/Frozen Lake/Frozen_Lake_Overview.png", "../assets/images/Ironwing/Frozen Lake/not_found.png"));
-dragons.push(new Dragon("HAERVAN CAVERNS", "Ironwing", "Epic", "3", "1000", "../assets/images/Ironwing/Haervan Caverns/Haervan_Caverns_Overview.png", "../assets/images/Ironwing/Haervan Caverns/Haervan_Caverns_Egg.png"));
-dragons.push(new Dragon("HELSTOR" || "HELSTÖR", "Bloodchaser", "Legendary", "0", "10000", "../assets/images/Bloodchaser/Helstor/Helstor_Overview.png", "../assets/images/Bloodchaser/Helstor/Helstor_Egg.png"));
-dragons.push(new Dragon("LAEGAN CHASMS", "Nightsnagger", "Epic", "0", "1000", "../assets/images/Nightsnagger/Laegan Chasms/Laegan_Chasms_Overview.png", "../assets/images/Nightsnagger/Laegan Chasms/Laegan_Chasms_Egg.png"));
-dragons.push(new Dragon("OLMUND", "Ironwing", "Legendary", "2", "10000", "../assets/images/Ironwing/Olmund/Olmund_Overview.png", "../assets/images/Ironwing/Olmund/Olmund_Egg.png"));
-dragons.push(new Dragon("RAGING RIVER", "Bloodchaser", "Epic", "11", "1000", "../assets/images/Bloodchaser/Raging River/Raging_River_Overview.png", "../assets/images/Bloodchaser/Raging River/not_found.png"));
-dragons.push(new Dragon("RAYLESS DEPTHS", "Ironwing", "Rare", "26", "500", "../assets/images/Ironwing/Rayless Depths/Rayless_Depths_Overview.png", "../assets/images/Ironwing/Rayless Depths/Rayless_Depths_Egg.png"));
-dragons.push(new Dragon("SYGGIAN FRINGES", "Ironwing", "Epic", "7", "1000", "../assets/images/Ironwing/Syggian Fringes/Syggian_Fringes_Overview.png", "../assets/images/Ironwing/Syggian Fringes/not_found.png"));
-dragons.push(new Dragon("TORRUNI DUNES", "Nightsnagger", "Epic", "5", "1000", "../assets/images/Nightsnagger/Torruni Dunes/Torruni_Dunes_Overview.png", "../assets/images/Nightsnagger/Torruni Dunes/not_found.png"));
-dragons.push(new Dragon("VENFIR", "Nightsnagger", "Legendary", "2", "1000", "../assets/images/Nightsnagger/Venfir/Venfir_Lingering_Dread_Overview.png", "../assets/images/Nightsnagger/Venfir/not_found.png"));
-dragons.push(new Dragon("WARELM", "Bloodchaser", "Legendary", "1", "1000", "../assets/images/Bloodchaser/Warelm/Warelm_Overview.png", "../assets/images/Bloodchaser/Warelm/not_found.png"));
+dragons.push(new Dragon(0, "ARID BADLANDS", "Bloodchaser", "Rare", 500, "../assets/images/Bloodchaser/Arid Badlands/Arid_Badlands_Overview.png", "../assets/images/Bloodchaser/Arid Badlands/Arid_Badlands_egg.png"));
+dragons.push(new Dragon(1, "FERROUS MOUNTAINS", "Bloodchaser", "Common", 100, "../assets/images/Bloodchaser/Ferrous Mountains/Ferrous_Mountains_Overview.png", "../assets/images/Bloodchaser/Ferrous Mountains/not_found.png"));
+dragons.push(new Dragon(2, "FJALNÄRRI MINES", "Nightsnagger", "Epic", 1000, "../assets/images/Nightsnagger/Fjalnarri Mines/Fjalnarri_Mines_Overview.png", "../assets/images/Nightsnagger/Fjalnarri Mines/not_found.png"));
+dragons.push(new Dragon(3, "FORGOTTEN BOG", "Nightsnagger", "Rare", 500, "../assets/images/Nightsnagger/Forgotten Bog/Forgotten_Bog_Overview.png", "../assets/images/Nightsnagger/Forgotten Bog/Forgotten_Bog_Egg.png"));
+dragons.push(new Dragon(4, "FROZEN LAKE", "Ironwing", "Epic", 1000, "../assets/images/Ironwing/Frozen Lake/Frozen_Lake_Overview.png", "../assets/images/Ironwing/Frozen Lake/not_found.png"));
+dragons.push(new Dragon(5, "HAERVAN CAVERNS", "Ironwing", "Epic", 1000, "../assets/images/Ironwing/Haervan Caverns/Haervan_Caverns_Overview.png", "../assets/images/Ironwing/Haervan Caverns/Haervan_Caverns_Egg.png"));
+dragons.push(new Dragon(6, "HELSTÖR", "Bloodchaser", "Legendary", 10000, "../assets/images/Bloodchaser/Helstor/Helstor_Overview.png", "../assets/images/Bloodchaser/Helstor/Helstor_Egg.png"));
+dragons.push(new Dragon(7, "LAEGAN CHASMS", "Nightsnagger", "Epic", 1000, "../assets/images/Nightsnagger/Laegan Chasms/Laegan_Chasms_Overview.png", "../assets/images/Nightsnagger/Laegan Chasms/Laegan_Chasms_Egg.png"));
+dragons.push(new Dragon(8, "OLMUND", "Ironwing", "Legendary", 10000, "../assets/images/Ironwing/Olmund/Olmund_Overview.png", "../assets/images/Ironwing/Olmund/Olmund_Egg.png"));
+dragons.push(new Dragon(9, "RAGING RIVER", "Bloodchaser", "Epic", 1000, "../assets/images/Bloodchaser/Raging River/Raging_River_Overview.png", "../assets/images/Bloodchaser/Raging River/not_found.png"));
+dragons.push(new Dragon(10, "RAYLESS DEPTHS", "Ironwing", "Rare", 500, "../assets/images/Ironwing/Rayless Depths/Rayless_Depths_Overview.png", "../assets/images/Ironwing/Rayless Depths/Rayless_Depths_Egg.png"));
+dragons.push(new Dragon(11, "SYGGIAN FRINGES", "Ironwing", "Epic", 1000, "../assets/images/Ironwing/Syggian Fringes/Syggian_Fringes_Overview.png", "../assets/images/Ironwing/Syggian Fringes/not_found.png"));
+dragons.push(new Dragon(12, "TORRUNI DUNES", "Nightsnagger", "Epic", 1000, "../assets/images/Nightsnagger/Torruni Dunes/Torruni_Dunes_Overview.png", "../assets/images/Nightsnagger/Torruni Dunes/not_found.png"));
+dragons.push(new Dragon(13, "VENFIR", "Nightsnagger", "Legendary", 1000, "../assets/images/Nightsnagger/Venfir/Venfir_Lingering_Dread_Overview.png", "../assets/images/Nightsnagger/Venfir/not_found.png"));
+dragons.push(new Dragon(14, "WARELM", "Bloodchaser", "Legendary", 1000, "../assets/images/Bloodchaser/Warelm/Warelm_Overview.png", "../assets/images/Bloodchaser/Warelm/not_found.png"));
 
-function stock() {
+//List the products.
+
+let shopContainer = document.getElementById(eggShop);
+
+
+for (const dragon of dragons) {
+    let card = document.createElement('div');
+    eggShop.appendChild(card);
+    card.className = 'card';
+    card.innerHTML =
+        `
+    <div class="card-inner">
+        <div class="card-front">
+            <h3 class="title">${dragon.name}</h3>
+            <img src="${dragon.overview}" class="img-fluid" alt="Overview image">
+            <p class="breed">${dragon.breed}</p>
+            <p id="rarity" class="rarity${dragon.rarity}">${dragon.rarity}</p>
+            <div class="price"><p>USD$${dragon.price}</p></div>
+        </div>
+        <div class="card-back">
+            <img src="${dragon.egg}" class="img-fluid" alt="Egg">
+            <div id="select${dragon.id}" class="select"><p>Add to Cart</p><div>
+        </div>
+    </div>
+    `;
+
+    let button = document.getElementById(`select${dragon.id}`);
+
+    button.onclick = () => addDragon(dragon.id);
+}
+
+//Check data in local storage.
+
+function checkLocalStorage() {
+    let contentInStorage = JSON.parse(localStorage.getItem('storedDragons'));
+
+    if (contentInStorage) {
+        for (const storedDragon of contentInStorage) {
+            let dragonToStore = dragons[storedDragon.id];
+            dragonToStore.quantity = storedDragon.quantity;
+            dragonToStore.totalPrice = storedDragon.totalPrice;
+        }
+        createTable(dragons);
+    }
+}
+
+//Add a Dragon to the cart.
+
+function addDragon(dragonID) {
+    let dragonChosen = dragons[dragonID];
+    dragonChosen.addUnit();
+    updateSelection();
+}
+
+//Remove a Dragon from the cart.
+
+function removeDragon(dragonID) {
+    let dragonRemoved = dragons[dragonID];
+    dragonRemoved.removeUnit();
+    updateSelection();
+}
+
+//Update the selection table.
+
+function updateSelection() {
+    localStorage.setItem('storedDragons', JSON.stringify(dragons));
+    createTable(dragons);
+}
+
+//Print the table on the HTML.
+
+function createTable(array) {
+    let tableContainer = document.getElementById('cart');
+    tableContainer.innerHTML = "";
+
+    let totalPrice = getTotalPrice(dragons);
+
+    let table = document.createElement('div');
+
+    table.innerHTML = `
+    <table id="cartTable" class="table">
+        <tbody id="tableBody">
+            <tr>
+                <td scope="col">Total: $${totalPrice}</td>
+                <td scope="col"></td>
+                <td scope="col"></td>
+                <td scope="col"></td>
+            </tr>
+            <tr>
+                <td><button id="clearSelection">Clear Cart</button></td>
+            </tr>
+        </tbody>
+    </table>
+    `
+    tableContainer.appendChild(table);
+
+    let tableBody = document.getElementById('tableBody');
+
+    for (let dragon of array) {
+        if (dragon.quantity > 0) {
+            let data = document.createElement('div');
+            data.innerHTML = `
+            <tr>
+                <th scope="row"></th>
+                <td scope="col">${dragon.name}</td>
+                <td scope="col">${dragon.quantity}</td>
+                <td scope="col">${dragon.totalPrice}</td>
+                <td scope="col"><button id="remove${dragon.id}" type="button"></button></td>
+            </tr>
+            `
+
+            tableBody.appendChild(data);
+
+            let removeBtn = document.getElementById(`remove${dragon.id}`);
+            removeBtn.addEventListener('click', () => removeDragon(dragon.id));
+
+            //Clear the cart.
+
+            let clearCartBtn = document.getElementById('clearSelection');
+            clearCartBtn.onclick = () => {
+                for (const dragon of dragons) {
+                    dragon.quantity = 0;
+                    dragon.totalPrice = 0;
+                }
+
+                updateSelection();
+            }
+        }
+    }
+}
+
+//Get the total price of the cart.
+
+function getTotalPrice() {
+    let totalPrice = 0;
+
+    for (const dragon of dragons) {
+        totalPrice += dragon.totalPrice;
+    }
+
+    return totalPrice;
+}
+
+checkLocalStorage();
+
+//Deprecated code.
+
+/* function stock() {
     for (let i = 0; i < dragons.length; i++) {
         var dragonName = dragons[i].getName();
         nameString += dragonName + "\n";
@@ -96,9 +225,9 @@ function stock() {
         var dragonName = dragons[i].getOverview();
         nameString += dragonName + "\n";
     }
-}
+} */
 
-function chooseDragon() {
+/* function chooseDragon() {
     stock();
     selection = prompt("Choose a Dragon to adopt. Dragons available:\n " + nameString).toUpperCase();
 }
@@ -125,33 +254,6 @@ function repeat() {
         alert("Please input a valid Dragon.");
         repeat();
     }
-}
+} */
 
 /* repeat(); */
-
-//List the products.
-
-let shopContainer = document.getElementById(eggShop);
-
-
-for (const dragon of dragons) {
-    let card = document.createElement('div');
-    eggShop.appendChild(card);
-    card.className = 'card';
-    card.innerHTML =
-        `
-    <div class="card-inner">
-        <div class="card-front">
-            <h3 class="title">${dragon.name}</h3>
-            <img src="${dragon.overview}" class="img-fluid" alt="Overview image">
-            <p class="breed">${dragon.breed}</p>
-            <p id="rarity" class="rarity${dragon.rarity}">${dragon.rarity}</p>
-            <div class="price"><p>USD$${dragon.price}</p></div>
-        </div>
-        <div class="card-back">
-            <img src="${dragon.egg}" class="img-fluid" alt="Egg">
-            <div class="select"><p>Add to Cart</p><div>
-        </div>
-    </div>
-    `
-}
