20dea2c4569a07738127b9d9500a984e07aed164
NeftisDragon <gregocm1000@gmail.com>
2022-04-27 20:19:51 -0300

    Updated SCSS (wip). Removed unused assets.

---
+++ scripts/header.js
@@ -0,0 +1,21 @@
+let dropdownTrigger = document.getElementById('dropdown-trigger');
+let dropdown = document.getElementById('dropdown');
+pos = false;
+
+dropdownTrigger.onclick = () => {
+    if (pos == false) {
+        dropdownTrigger.classList.add('dropdown-trigger-up');
+
+        dropdown.classList.add('dropdown-shown');
+        dropdown.classList.remove('dropdown-hidden');
+        
+        pos = true;
+    } else {
+        dropdownTrigger.classList.remove('dropdown-trigger-up');
+
+        dropdown.classList.remove('dropdown-shown');
+        dropdown.classList.add('dropdown-hidden');
+        
+        pos = false;
+    }
+}
